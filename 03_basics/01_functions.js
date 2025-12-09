70928927adadf94d5f7c98acec84c6c294c0fb92
Vivek Kumar <vivekbarwal1982@gmail.com>
2025-12-09 21:01:48 +0530

    function with objects and arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -38,9 +38,62 @@
 // console.log(loginUserMessage())        // undefined
 
 
-function loginUserMessage(username="sam"){
-    return `${username} just logged in `
+// function loginUserMessage(username="sam"){
+//     return `${username} just logged in `
+// }
+
+// console.log(loginUserMessage("vivek"))    //vivek just logged in
+// console.log(loginUserMessage())        // sam just logged in
+
+
+
+
+
+
+
+// +++++++++++++++++ PART 2  +++++++++++++++++++++++
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(200,300,400));    //[ 200, 300, 400 ] -> now we get array , we can perform our desired operations on it
+
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,300,400,500,600));  //[ 400, 500, 600 ]
+
+
+
+
+
+
+
+// function with object
+const user={
+    username:"vivek",
+    prices:199
 }
 
-console.log(loginUserMessage("vivek"))    //vivek just logged in
-console.log(loginUserMessage())        // sam just logged in
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject(user)     //Username is vivek and price is undefined
+
+
+
+
+
+
+
+
+// function with array
+
+const myarray=[100,200,300,400,500]
+
+function returnsecondvalue(getarray){
+    return getarray[1]
+}
+// console.log(`second value is ${returnsecondvalue(myarray)}`);          output:- 200 
+console.log(`second value is ${returnsecondvalue([100,200,300,400,500])}`);      // output:- 200
