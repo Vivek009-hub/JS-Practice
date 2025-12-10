865c7aaea28fa6abee2f848256445e61177b5358
Vivek Kumar <vivekbarwal1982@gmail.com>
2025-12-10 14:53:57 +0530

    scopes done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,21 +1,66 @@
-if(true){
-    let a =10
-    const b=20
-    var c=30
-}
-// console.log(a);    // error dega
-// console.log(b);    // error dega
-console.log(c);       // 30
+// if(true){
+//     let a =10
+//     const b=20
+//     var c=30
+// }
+// // console.log(a);    // error dega
+// // console.log(b);    // error dega
+// console.log(c);       // 30
 
 
 
-let a = 100
-if(true){
-    let a =10
-    const b=20
+// let a = 100
+// if(true){
+//     let a =10
+//     const b=20
 
-    console.log('Inner a:',a);
+//     console.log('Inner a:',a);
     
-}
-console.log('Outer a:',a);
+// }
+// console.log('Outer a:',a);
 
+
+
+
+
+
+
+// NOTE:-  Outside cannot access inside but inside can access outside very well
+function one(){
+    const username="vivek"
+
+    function two(){
+        const website ="youtube"
+        console.log(username);   // vivek ( we can access outside element)
+    }
+    // console.log(website);     // will give error ( cant access outside block )
+
+    two()   
+}
+one()
+
+
+
+
+
+
+
+
+//  Two methods of declaring functions 
+console.log(addOne(5))  // 6 ( will work normally)
+
+function addOne(num){
+    return num+1
+}
+console.log(addOne(5))  // 6 ( will work normally)
+
+
+
+
+
+// console.log(addTwo(5));    // Error ( trying to access variable before intialization)
+
+const addTwo = function(num){     // here we have stored function in a variable
+    return num+2
+}
+console.log(addTwo(5));    // 7
--- 03_basics/tempCodeRunnerFile.js
+++ 03_basics/tempCodeRunnerFile.js
@@ -1,8 +1,1 @@
-if(true){
-//     let a =10
-//     const b=20
-//     var c=30
-// }
-// // console.log(a);    // error dega
-// // console.log(b);    // error dega
-// console.log(c);       // 30
+console.log(addTwo(5));    // Error ( trying to access variable before intialization)
