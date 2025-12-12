3ca223b29129692d29f90f1606738c4f890cddd6
Vivek Kumar <vivekbarwal1982@gmail.com>
2025-12-12 19:17:46 +0530

    control flow

---
+++ 04_control_flow.js/one.js
@@ -0,0 +1,10 @@
+// let age = prompt("Enter your age")    // this input method works only in Browser
+// if(age>18){
+//     const a = 10
+//     console.log(`your age is: ${age} and your number is ${a}`);
+// }
+
+
+
+
+// IF ELSE Condition  
---
+++ 04_control_flow.js/switch.js
@@ -0,0 +1,35 @@
+// Switch case 
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log('January');
+
+        break;
+    case 2:
+        console.log('Feb');
+        break
+    case 3:
+        console.log('Mar');
+        // break              // now print all remaining cases except Default case
+    case 3:
+        console.log('Apr');
+        break
+    default:
+        console.log('default case mathched');
+
+        break;
+}
+
+
+// Switch can be used with string also
